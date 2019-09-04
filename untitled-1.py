07ff40370020bd8f98e5f85a1291a244b7fba8c8
anngocle <52774371+anngocle@users.noreply.github.com>
2019-09-04 12:58:45 +0700

    1

---
+++ untitled-1.py
@@ -0,0 +1,10 @@
+a = " Hello,World! "
+print(a.lower())
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(1, "orange")
+print (thislist[-1])
+print(thislist)
+thislist.clear()
+print("hello")
+print ("lengocan")
+    
