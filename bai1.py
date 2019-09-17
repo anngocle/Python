57a2781356941ad47e40947e82715c9d3afe471f
anngocle <52774371+anngocle@users.noreply.github.com>
2019-09-17 11:38:29 +0700

    v3
    v3.0

--- bai1.py
+++ bai1.py
@@ -1,7 +1,12 @@
 # trust fund
-b ="lengocan"
-b = int(b)
-print (b)
-
-
+# import random
+b =0
+while True:
+    print("helllo")
+    b = b+1
+    if b==5:
+        continue 
+    print (b)
+         
+print(" enter key to exit")   
 
